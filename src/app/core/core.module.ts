78773a3f06c0895cce9fecab2e754fbbf388e7e4
IvoAtanasov <ivoatanasov85@gmail.com>
2018-02-10 21:44:03 +0200

    add movie service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MoviePosterComponent } from '../movie-poster/movie-poster.component';
+import { MovieService } from './movie.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [HeaderComponent, MoviePosterComponent],
-  exports: [HeaderComponent, MoviePosterComponent]
+  exports: [HeaderComponent, MoviePosterComponent],
+  providers: [MovieService]
 })
 export class CoreModule { }
